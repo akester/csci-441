4876df0a7f6c38602062e3f63008a60c06022cfb
Andrew Kester <andrew@sthse.co>
2021-02-22 23:32:30 -0600

    Remove test commit

--- config/database.php
+++ config/database.php
@@ -31,8 +31,6 @@ if ($config->isValidPlatform() && $config->inRuntime()) {
     $mysql[0]['host'] = $credentials['host'];
     $mysql[0]['port'] = $credentials['port'];
     $mysql[0]['database'] = $credentials['path'];
-
-    dd ($mysql);
 }
 
 return [
