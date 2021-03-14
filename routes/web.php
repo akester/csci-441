9817c43d6201451c97a39f2970c7c38f87b765ac
Andrew Kester <andrew@sthse.co>
2021-03-14 17:31:23 -0500

    Add base home page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    if (Auth::user()) {
+        return redirect ('/dashboard');
+    }
     return view('welcome');
 });
 
