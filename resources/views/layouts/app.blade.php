908f52293c66cf590df19795505ed4fb02e21143
Andrew Kester <aikester@aikester.com>
2022-05-06 00:20:09 -0500

    Add links to logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,11 @@
 <div id="container"> <!--Container has all 8 areas of the viewport-->
     <!--The top sector, It takes all the top part  -->
     <header>
-        <div id="logo"><img src="{{ asset('img/header.png') }}"></div>
+        <div id="logo">
+            <a href="/dashboard">
+                <img src="{{ asset('img/header.png') }}">
+            </a>
+        </div>
     </header>
 
     <!--Left side right below header - nothing on it at the moment-->
