86ceff8f4ba95e2cf6239291f7f0b36ff50bdec7
Andrew Kester <aikester@aikester.com>
2022-05-05 22:37:41 -0500

    Add new row, move up, move down

--- resources/js/components/Editor.vue
+++ resources/js/components/Editor.vue
@@ -5,13 +5,17 @@
             <table id="editor-table">
                 <tr v-for="(bookmark,index) in bookmarks" :key="bookmark.id">
                     <td :class="['title', 'level-' + bookmark.level]"><input type="text" v-model="bookmark.title" /></td>
-                    <td>
-                        <button @click="bookmark.level--" class="promote"></button>
+                    <td class="actions">
+                        <button @click="bookmark.level--" v-if="bookmark.level > 1" class="promote"></button>
                         <button @click="bookmark.level++" class="demote"></button>
+                        <button @click="swap(index, index-1)" v-if="index > 0" class="move-up"></button>
+                        <button @click="swap(index, index+1)" v-if="index < bookmarks.length - 1" class="move-down"></button>
                         <button @click="bookmarks.splice(index, 1)" class="delete"></button>
                     </td>
                 </tr>
             </table>
+
+            <button @click="bookmarks.push({level: 1, title: ''})">Add Row</button>
         </div>
     </div>
 </template>
@@ -29,6 +33,24 @@
             // This loads off an object set on the page from Laravel
             this.metadata = metadata;
             this.bookmarks = this.metadata.bookmarks;
-        }
+        },
+
+        methods: {
+            swap: function(x, y) {
+                if (x < 0 || y < 0) {
+                    // Keep us from swapping negative, which would cause index issues
+                    return;
+                }
+
+                if (x > this.bookmarks.length - 1 || y > this.bookmarks.length - 1) {
+                    // Keep us from swapping outside the array
+                    return;
+                }
+
+                var temp = this.bookmarks[x]
+                this.bookmarks[x] = this.bookmarks[y]
+                this.bookmarks[y] = temp
+            }
+        },
     }
 </script>
